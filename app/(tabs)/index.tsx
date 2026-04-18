a4b0c50d55be2c322b305c6a0d83960a54667fec
Anna Milewski <annamilewski@MacBook-Pro-von-Anna.local>
2026-04-18 15:03:50 +0300

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,13 +7,13 @@ const SafeAreaView = styled(RNSafeAreaView)
 export default function App() {
     return (
         <SafeAreaView className="flex-1 bg-background p-5">
-            <Text className="text-xl font-bold text-success">
-                Welcome to Nativewind!
+            <Text className="text-5xl font-sans-extrabold">
+             Home
             </Text>
             <Link href= "/onboarding" className="mt-4 rounded bg-primary text-white p-4">Go to Onbording</Link>
             <Link href= "/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">Sign In</Link>
             <Link href= "/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Sign Up</Link>
-            <Link href="./app/subscriptions/spotify">Spotify Subscriptions</Link>
+            <Link href="/subscriptions/spotify">Spotify Subscriptions</Link>
             <Link
             href={{
                 pathname: "/subscriptions/[id]",
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,22 @@
 import { Stack } from "expo-router";
 import "@/global.css"
+import {useFonts} from "expo-font";
+import {useEffect} from "react";
+import {SplashScreen} from "expo-router";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "sans-regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "sans-medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "sans-semibold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "sans-bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "sans-light": require("../assets/fonts/Quicksand-Light.ttf"),
+  });
+  useEffect(() => {
+    if(fontsLoaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+  if(!fontsLoaded) return  null;
   return <Stack screenOptions={{headerShown: false}} />;
 }
